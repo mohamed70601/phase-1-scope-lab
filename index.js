866880902170e69eb5db5bae0a503547e8325ed1
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-08-10 03:24:39 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Dwight';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'another one';
+}
